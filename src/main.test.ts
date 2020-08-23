14e588ec87841ab2d96b559ed72a231219832417
Cameron Little <cameron@camlittle.com>
2020-08-23 16:37:21 +0200

    fix test

--- src/main.test.ts
+++ src/main.test.ts
@@ -92,7 +92,7 @@ describe("test suite", () => {
   });
 
   function assertActivationBehavior() {
-    expect(nova.commands.register).toBeCalledTimes(4);
+    expect(nova.commands.register).toBeCalledTimes(5);
     expect(nova.commands.register).toBeCalledWith(
       "apexskier.typescript.goToDefinition",
       expect.any(Function)
@@ -107,6 +107,10 @@ describe("test suite", () => {
     );
     expect(nova.commands.register).toBeCalledWith(
       "apexskier.typescript.findSymbol",
+      expect.any(Function)
+    );
+    expect(nova.commands.register).toBeCalledWith(
+      "apexskier.typescript.autoSuggest",
       expect.any(Function)
     );
 
