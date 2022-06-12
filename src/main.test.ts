a6c9727757ca8637e1345f774ad4447f0f306efc
Cameron Little <cameron@camlittle.com>
2022-06-12 17:04:11 -0400

    update tests

--- src/main.test.ts
+++ src/main.test.ts
@@ -199,7 +199,7 @@ describe("test suite", () => {
       },
       {
         initializationOptions: { preferences: "user preferences" },
-        syntaxes: ["typescript", "tsx", "javascript", "jsx", "cts", "mts"],
+        syntaxes: ["typescript", "tsx", "cts", "mts", "javascript", "jsx"],
       }
     );
     const languageClient: LanguageClient =
